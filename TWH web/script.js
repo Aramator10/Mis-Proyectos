dee837802abb604adc667cee743948fef5a3855b
Aram Rodriguez <145541881+Aramator10@users.noreply.github.com>
2024-12-12 10:38:50 +0100

    Add files via upload

---
+++ TWH web/script.js
@@ -0,0 +1,21 @@
+// Smooth scroll para los enlaces de navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Verificar si debe recargar la página
+window.onload = function() {
+    if (localStorage.getItem('shouldReload')) {
+        localStorage.removeItem('shouldReload');
+        window.location.reload();
+    }
+};
+
+document.getElementById('background-audio').onloadedmetadata = function() {
+    this.currentTime = 12.8;
+};
